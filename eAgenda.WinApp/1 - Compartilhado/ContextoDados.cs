1d30e2bf7102efee644b8b41e054c3a64f2655bf
Leonardo Rodrigues <himata666@hotmail.com>
2024-07-01 23:24:06 -0300

    Primeiro commit

---
+++ eAgenda.WinApp/1 - Compartilhado/ContextoDados.cs
@@ -0,0 +1,59 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace eAgenda.WinApp.Compartilhado
+{
+    public class ContextoDados
+    {
+        private string caminho = Directory.GetCurrentDirectory().Split("bin")[0] + "\\Dados\\Data.json";
+
+        public ContextoDados()
+        {
+        }
+
+        public ContextoDados(bool carregarDados) : this()
+        {
+            if (carregarDados)
+                CarregarDados();
+        }
+
+        public void Gravar()
+        {
+            FileInfo arquivo = new FileInfo(caminho);
+
+            arquivo.Directory.Create();
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+                ReferenceHandler = ReferenceHandler.Preserve
+            };
+
+            byte[] registrosEmBytes = JsonSerializer.SerializeToUtf8Bytes(this, options);
+
+            File.WriteAllBytes(caminho, registrosEmBytes);
+        }
+
+        protected void CarregarDados()
+        {
+            FileInfo arquivo = new FileInfo(caminho);
+
+            if (!arquivo.Exists)
+                return;
+
+            byte[] registrosEmBytes = File.ReadAllBytes(caminho);
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
+            };
+
+            ContextoDados ctx = JsonSerializer.Deserialize<ContextoDados>(registrosEmBytes, options);
+
+            if (ctx == null)
+                return;
+        }
+    }
+}
+}
---
+++ eAgenda.WinApp/1 - Compartilhado/IControladorFiltravel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eAgenda.WinApp._1___Compartilhado
+{
+    public interface IControladorFiltravel
+    {
+
+        string ToolTipFiltravel { get; }
+
+        void Filtrar();
+    }
+}
